f7a6498db8734e6be437fbdc8c957840a4673c55
hyder147 <alihyderr14@gmail.com>
2025-12-20 19:08:55 +0500

    Add project files.

---
+++ CircleApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using CircleApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CircleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
